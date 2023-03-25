b1a7a89632996ce1d6ce07074559563a11de573b
sachincharakhwal@gmail.com <sachincharakhwal@gmail.com>
2023-03-26 02:13:00 +0530

    add `nvim-cmp` config

--- chadrc.lua
+++ chadrc.lua
@@ -6,10 +6,16 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "onedark",
-  theme_toggle = { "onedark", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "atom", -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
+    selected_item_bg = "simple", -- colored / simple
+  },
 }
 
 M.plugins = "custom.plugins"
