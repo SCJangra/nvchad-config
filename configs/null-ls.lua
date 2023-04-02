cbf8225e04d739a9af671b17c79c69b08822ad4b
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-04-02 17:14:12 +0530

    auto format rust

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -1,28 +1,31 @@
-local present, null_ls = pcall(require, "null-ls")
+local present, null_ls = pcall(require, 'null-ls')
 
 if not present then
-	return
+  return
 end
 
 local b = null_ls.builtins
 
 local sources = {
-	b.formatting.stylua,
-	b.formatting.prettierd,
+  b.formatting.stylua,
+  b.formatting.prettierd,
+  b.formatting.rustfmt.with {
+    extra_args = { '--edition=2021' },
+  },
 }
 
-null_ls.setup({
-	debug = true,
-	sources = sources,
-	-- you can reuse a shared lspconfig on_attach callback here
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ buffer = bufnr })
-				end,
-			})
-		end
-	end,
-})
+null_ls.setup {
+  debug = true,
+  sources = sources,
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+    if client.supports_method 'textDocument/formatting' then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { buffer = bufnr }
+        end,
+      })
+    end
+  end,
+}
