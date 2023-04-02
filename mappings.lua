3ec6bf308a72e18aed8fb39f8531f78f70859798
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-04-03 01:38:24 +0530

    add mappings
    * to yank to clipboard
    * to redo using `U`

--- mappings.lua
+++ mappings.lua
@@ -29,12 +29,14 @@ set('n', '<leader>q', '<CMD>q<CR>', { desc = 'Close window' })
 set('n', '<leader>w', '<CMD>w<CR>', { desc = 'Save file' })
 
 -- Misc
+set('n', 'U', '<C-r>', { desc = 'Redo' })
 set('n', '<leader>d', '<CMD>DiffviewOpen<CR>', { desc = 'DiffView' })
 set('n', '<leader>e', '<CMD>NvimTreeToggle<CR>', { desc = 'Explorer' })
 set('n', '<leader>f', '<CMD>Telescope find_files<CR>', { desc = 'Find files' })
 set('n', '<leader>g', '<CMD>lua require("neogit").open()<CR>', { desc = 'Neogit' })
 set('n', '<leader>u', '<CMD>NvChadUpdate<CR>', { desc = 'Update NvChad' })
 set('t', '<C-x>', termcodes '<C-\\><C-N>', { desc = '   escape terminal mode' })
+set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Yank to clipboard' })
 
 -- LSP
 set('n', 'gd', '<CMD>lua vim.lsp.buf.definition()<CR>', { desc = 'LSP definition' })
