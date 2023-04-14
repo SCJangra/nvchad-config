25ce83159895666ef33398e303ffce51317e0005
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-04-15 00:09:46 +0530

    chore: auto format

--- chadrc.lua
+++ chadrc.lua
@@ -2,25 +2,25 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
+local highlights = require 'custom.highlights'
 
 M.ui = {
-  theme = "everblush",
+  theme = 'everblush',
 
   hl_override = highlights.override,
   hl_add = highlights.add,
   cmp = {
     icons = true,
     lspkind_text = true,
-    style = "atom", -- default/flat_light/flat_dark/atom/atom_colored
-    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
-    selected_item_bg = "simple", -- colored / simple
+    style = 'atom', -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = 'grey_fg', -- only applicable for "default" style, use color names from base30 variables
+    selected_item_bg = 'colored', -- colored / simple
   },
 }
 
-M.plugins = "custom.plugins"
+M.plugins = 'custom.plugins'
 
 -- check core.mappings for table structure
-M.mappings = require "custom.disabled_mappings"
+M.mappings = require 'custom.disabled_mappings'
 
 return M
