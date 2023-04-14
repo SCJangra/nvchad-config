4581ca164ca35a832e7510df6bf8b6b6de49b86a
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-04-15 00:06:05 +0530

    feat(gitsigns): all custom symbols

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -4,6 +4,10 @@ M.gitsigns = {
   signs = {
     add = { text = '▌' },
     change = { text = '▌' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked = { text = '🮌' },
   },
 }
 
