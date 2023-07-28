6c3b0f3697995011108c8d0d66d2f143bd3d18bf
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-07-28 17:01:58 +0530

    plugin(dressing): change popup opacity

--- configs/dressing.lua
+++ configs/dressing.lua
@@ -1,24 +1,35 @@
+local options = {}
+
+options.select = {
+  backend = { 'builtin' },
+  builtin = {
+    win_options = {
+      winblend = 0,
+    },
+  },
+  get_config = function(opts)
+    if opts.kind == 'codeaction' then return {
+      builtin = {
+        relative = 'cursor',
+      },
+    } end
+  end,
+}
+
+options.input = {
+  prefer_width = 50,
+  insert_only = false,
+  win_options = {
+    winblend = 0,
+  },
+  get_config = function(opts)
+    if opts.center then return { relative = 'win' } end
+  end,
+}
+
 return {
   'stevearc/dressing.nvim',
   -- TODO: Lazy load this
   lazy = false,
-  opts = {
-    select = {
-      backend = { 'builtin' },
-      get_config = function(opts)
-        if opts.kind == 'codeaction' then
-          return {
-            builtin = {
-              relative = 'cursor',
-            },
-          }
-        end
-      end,
-    },
-    input = {
-      get_config = function(opts)
-        if opts.center then return { relative = 'win' } end
-      end,
-    },
-  },
+  opts = options,
 }
